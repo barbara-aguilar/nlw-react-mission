8f9d2ea34cf2180a5ed3ec00003f17f32bf5e357
Barbara Aguilar <Bah@Barbaras-MacBook-Pro.local>
2021-06-22 22:30:46 -0300

    create scss styles, create pages, add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import { Button } from './components/Button'
+import {BrowserRouter, Route} from 'react-router-dom'
+
+import { Home } from './pages/Home'
+import { NewRoom } from './pages/NewRoom'
+import './styles/global.scss'
+
 function App() {
   return (
-    <Button />
-  );
+    <BrowserRouter>
+    <Route path="/" exact component={Home} />
+    <Route path="/rooms/new" component={NewRoom} />
+    </BrowserRouter>
+    );
 }
 
 export default App;
